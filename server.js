76b18956eff23aa78312353ab33a85d2fe818736
avi4rag <avi4rag@gmail.com>
2026-07-20 20:36:21 +0530

    Setup Express server and MongoDB connection

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+const todoRoutes = require("./routes/todoRoutes");
+
+const app = express();
+
+
+app.use(cors()); 
+app.use(express.json()); 
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", todoRoutes);
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error("MongoDB connection error:", err));
